d43544fa02f0a3bdb1fe448cfe74c82f9e96b3c5
Kristian <kristianuruplarsen@gmail.com>
2019-05-21 14:19:28 +0200

    cleanup and work

--- examproject/examproject/problem_one.py
+++ examproject/examproject/problem_one.py
@@ -1,6 +1,6 @@
 
 import numpy as np 
-
+import matplotlib.pyplot as plt 
 
 rho = 2
 beta = 0.96
@@ -72,3 +72,86 @@ def human_capital_accumulation(h1: float, l1: bool, rng: np.random.RandomState):
         float
     '''
     return h1 + l1 + (rng.uniform() > 0.5)*Delta
+
+
+
+
+
+def plot_period_2_solution(h_vec, solutions):
+    ''' Plot for question 1
+    '''
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize = (12,4))
+
+    ax1.plot(h_vec, [s[0] for s in solutions])
+    ax1.set_title('Labor supply', fontsize = 18)
+    ax1.set_xlabel(r'$h_2$', fontsize = 18)
+    ax1.set_ylabel(r'$l_2$', fontsize = 18)
+
+
+
+    ax2.plot(h_vec, [s[1] for s in solutions])
+    ax2.set_title('Utility', fontsize = 18)
+    ax2.set_xlabel(r'$h_2$', fontsize = 18)
+    ax2.set_ylabel(r'$v_2^*$', fontsize = 18)
+
+    ax1.grid(which='major', alpha=0.8, linestyle='dotted')
+    ax2.grid(which='major', alpha=0.8, linestyle='dotted')
+
+    fig.suptitle("Period 2 solution", fontsize = 24, y = 1.05)
+    plt.tight_layout()
+    plt.legend()
+    return fig
+
+
+
+def plot_period_1_solution(h_vec, solutions):
+    ''' Plot for question 2
+    '''
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize = (12,4))
+
+    ax1.plot(h_vec, [s[0] for s in solutions])
+    ax1.set_title('Labor supply', fontsize = 18)
+    ax1.set_xlabel(r'$h_1$', fontsize = 18)
+    ax1.set_ylabel(r'$l_1$', fontsize = 18)
+
+    ax2.plot(h_vec, [s[1] for s in solutions])
+    ax2.set_title('Utility', fontsize = 18)
+    ax2.set_xlabel(r'$h_1$', fontsize = 18)
+    ax2.set_ylabel(r'$v_1^*$', fontsize = 18)
+
+    ax1.grid(which='major', alpha=0.8, linestyle='dotted')
+    ax2.grid(which='major', alpha=0.8, linestyle='dotted')
+
+    fig.suptitle("Period 1 solution", fontsize = 24, y = 1.05)
+    plt.tight_layout()
+    
+    return fig
+
+
+def plot_period_1_solution_with_vbands(h_vec, solutions, lowest_work_solution):
+    ''' Plot for question 3
+    '''
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize = (12,4))
+
+    ax1.plot([w*h - b for h in h_vec], [s[0] for s in solutions])
+    ax1.axvspan(w*lowest_work_solution-b, 0, alpha=0.3, color='blue')
+    ax1.axvline(w*lowest_work_solution-b, color = 'black')
+    ax1.axvline(0, color = 'black')
+    ax1.set_title('Labor supply', fontsize = 18)
+    ax1.set_xlabel(r'$w\cdot h_1 - b$', fontsize = 18)
+    ax1.set_ylabel(r'$l_1$', fontsize = 18)
+
+    ax2.plot([w*h - b for h in h_vec], [s[1] for s in solutions])
+    ax2.axvspan(w*lowest_work_solution-b, 0, alpha=0.3, color='blue')
+    ax2.axvline(w*lowest_work_solution-b, color = 'black')
+    ax2.axvline(0, color = 'black')
+    ax2.set_title('Utility', fontsize = 18)
+    ax2.set_xlabel(r'$w\cdot h_1 - b$', fontsize = 18)
+    ax2.set_ylabel(r'$v_1^*$', fontsize = 18)
+
+    ax1.grid(which='major', alpha=0.8, linestyle='dotted')
+    ax2.grid(which='major', alpha=0.8, linestyle='dotted')
+
+    fig.suptitle("Period 1 solution", fontsize = 24, y = 1.05)
+    plt.tight_layout()
+    return fig
--- examproject/examproject/problem_two.py
+++ examproject/examproject/problem_two.py
@@ -1,5 +1,60 @@
 
 import numpy as np 
 import sympy as sp
+import matplotlib.pyplot as plt 
+
+def func_vt(v_tm1, x_t, delta):
+    'v_t = delta*v_{t-1} + x_t'
+    return delta*v_tm1 + x_t
+
+def func_st(s_tm1, c_t, omega):
+    's_t = omega*s_{t-1} + c_t'
+    return omega*s_tm1 + c_t
 
 
+def plot_single_shock(y_vec, pi_vec):
+    ''' Plot for question 3
+    '''
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    ax.plot(y_vec, color = 'red', label= '$y^*_t$')
+    ax.plot(pi_vec, color = 'blue', label = '$\pi^*_t$')
+
+    ax.grid(which='major', alpha=0.8, linestyle='dotted')
+    ax.set_xlabel('$t$', fontsize = 18)
+    ax.set_ylabel('$y^*_t$, $\pi^*_t$', fontsize = 18)
+    ax.set_title("Evolution of $y^*$, $\pi^*$", fontsize = 20)
+    ax.legend(fontsize = 18)
+    
+    return fig
+
+
+def plot_random_shocks(y_vec, pi_vec):
+    ''' Figure for question 4
+    '''
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    ax.plot(y_vec, color = 'red', label= '$y^*_t$', linewidth=0.5)
+    ax.plot(pi_vec, color = 'blue', label = '$\pi^*_t$', linewidth = 0.5)
+
+    ax.grid(which='major', alpha=0.8, linestyle='dotted')
+    ax.set_xlabel('$t$', fontsize = 18)
+    ax.set_ylabel('$y^*_t$, $\pi^*_t$', fontsize = 18)
+    ax.set_title("Evolution of $y^*$, $\pi^*$", fontsize = 20)
+    ax.legend(fontsize = 18)
+    
+    return fig
+
+
+
+def plot_correlation_and_phi(phi_space, correlation):
+    ''' Figure for question 5
+    '''
+    fig, ax = plt.subplots(figsize = (12,8))
+    ax.plot(phi_space, correlation, color = 'blue')
+
+    ax.grid(which='major', alpha=0.8, linestyle='dotted')
+    ax.set_xlabel('$\phi$', fontsize = 18)
+    ax.set_ylabel('$corr(y^*_t, \pi^*_t)$', fontsize = 18)
+    ax.set_title("Correlation btwn. $y^*_t, \pi^*_t$ for varying $\phi$", fontsize = 20)
+    return fig
